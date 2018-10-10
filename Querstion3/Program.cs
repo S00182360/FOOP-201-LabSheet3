8403d0f28a39d0efb8e312e94f955e53bb57c0d8
Sam Colville <scolville47@gmail.com>
2018-10-10 23:10:01 +0100

    Completed DemoSquares

---
+++ Querstion3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Querstion3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double sideLength = 0;
+            DemoSquares[] SquareArray = new DemoSquares[10];
+            //double area = 0;
+
+            for (int i = 0; i < SquareArray.Length; i++)
+            {
+                sideLength = i + 1;
+                SquareArray[i] = new DemoSquares(sideLength);
+                //SquareArray[i].Area = sideLength;
+                Console.WriteLine("The area of a square with length {0}M is {1}m^2", SquareArray[i].Length, SquareArray[i].Area);
+            }
+            //SquareArray[0].Area = sideLength;
+
+
+
+        }
+    }
+
+    public class DemoSquares
+    {
+        public DemoSquares(double length)
+        {
+            Length = length;
+        }
+
+        public double Length { get; }
+
+        private double _area;
+        public double Area
+        {
+            get {
+                _area = Length*Length;
+                return _area; }
+            set { _area = value; }
+
+        }
+
+        //private double AreaCalc(double length)
+        //{
+        //    Area = length * length;
+        //    return Area;
+        //}
+
+        //private double _perimiter;
+        //public double Perimiter
+        //{
+            //get { return _perimiter; }
+            //set { _perimiter = value * 4; }
+        //}
+
+
+
+    }
+}
